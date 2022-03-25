4646765b5af7c7593bf907ee7a9955d69ecee087
JunHatanaka <nextseason6@gmail.com>
2022-03-26 00:27:43 +0900

    Fix: prettier実行

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
+import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
-import '../../src/App.css'
+import "../../src/App.css";
 
 export const AddForm = (props) => {
-    const {
-        taskTitleText, 
-        onChangeTaskTitleText, 
-        taskStatus, 
-        onChangeTaskStatus, 
-        taskDetailText, 
-        onChangeTaskDetailText, 
-        onClickAdd,
-        date,
-        handleChange
-    } = props;
-    return (
-        <div className="add-style">
-            <form action="">
-                <input className="add-input-style" type="text" value={taskTitleText} onChange={onChangeTaskTitleText} maxLength='10' placeholder="タイトルを入力" />
-                <select className="add-input-style" name="" id="" value={taskStatus} onChange={onChangeTaskStatus}>
-                    <option value="">-</option>
-                    <option value="未着手">未着手</option>
-                    <option value="進行中">進行中</option>
-                    <option value="完了">完了</option>
-                </select>
-                <input className="add-input-detail-style" type="text" value={taskDetailText} onChange={onChangeTaskDetailText} maxLength='50' placeholder="タスクの詳細を入力" />
-                <div className="datepiker-wrap">
-                    <label>期限を入力</label>
-                    <DatePicker
-                        selected={date}
-                        onChange={handleChange}
-                        className="datepiker-input-style"
-                        dateFormat="yyyy/MM/dd"
-                    />
-                </div>
-                <button type="button" className="add-button-style" onClick={onClickAdd}>追加</button>
-            </form>
+  const {
+    taskTitleText,
+    onChangeTaskTitleText,
+    taskStatus,
+    onChangeTaskStatus,
+    taskDetailText,
+    onChangeTaskDetailText,
+    onClickAdd,
+    date,
+    handleChange,
+  } = props;
+  return (
+    <div className="add-style">
+      <form action="">
+        <input
+          className="add-input-style"
+          type="text"
+          value={taskTitleText}
+          onChange={onChangeTaskTitleText}
+          maxLength="10"
+          placeholder="タイトルを入力"
+        />
+        <select
+          className="add-input-style"
+          name=""
+          id=""
+          value={taskStatus}
+          onChange={onChangeTaskStatus}
+        >
+          <option value="">-</option>
+          <option value="未着手">未着手</option>
+          <option value="進行中">進行中</option>
+          <option value="完了">完了</option>
+        </select>
+        <input
+          className="add-input-detail-style"
+          type="text"
+          value={taskDetailText}
+          onChange={onChangeTaskDetailText}
+          maxLength="50"
+          placeholder="タスクの詳細を入力"
+        />
+        <div className="datepiker-wrap">
+          <label>期限を入力</label>
+          <DatePicker
+            selected={date}
+            onChange={handleChange}
+            className="datepiker-input-style"
+            dateFormat="yyyy/MM/dd"
+          />
         </div>
-    )
-}
+        <button type="button" className="add-button-style" onClick={onClickAdd}>
+          追加
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/components/TaskArea.jsx
+++ src/components/TaskArea.jsx
@@ -1,107 +1,199 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 Modal.setAppElement("#root");
 
 export const TaskArea = (props) => {
-    const {
-        todos, 
-        onChangeExistingTaskStatus, 
-        onClickDelete, 
-        ON_EDIT_FLG_TRUE, 
-        onClickEdit,
-        onClickBackEdit,
-        onChangeEditTaskTitleText,
-        onChangeEditTaskDetailText,
-        onClickUpdateTask,
-        modalIsOpen,
-        setIsOpen,
-        comments,
-        onClickAddComment,
-        commentContentText,
-        onChangeCommentContentText,
-        onClickOpenComment,
-        activeCommentParent
-    } = props;
-    return (
-        <div>
-        <ul>
-            {todos.map((todo, index)=>{
-                let taskStyle = {};
-                if (todo.status === "未着手") {
-                    taskStyle = 'status-not-started-yet-task-style';
-                } else if (todo.status === "進行中") {
-                    taskStyle = 'status-in-progress-task-style';
-                } else {
-                    taskStyle = 'status-complete-task-style';
-                }
-                if(todo.editFlg === ON_EDIT_FLG_TRUE){
-                    return (
-                        <li key={index} style={{listStyle: 'none'}}>
-                            <div className={taskStyle}>
-                                <p className="task-id-style">{todo.id}</p>
-                                <input id={`task-${todo.id}-title`} type="text" className="add-input-style" defaultValue={todo.title} placeholder="タイトル" maxLength='10' onChange={onChangeEditTaskTitleText} />
-                                <select 
-                                    id={`task-${todo.id}-status`} 
-                                    className="task-status-style"
-                                    value={todo.status} onChange={() => {onChangeExistingTaskStatus(todo.id, document.getElementById(`task-${todo.id}-status`).value)}}
-                                >
-                                    <option value="未着手">未着手</option>
-                                    <option value="進行中">進行中</option>
-                                    <option value="完了">完了</option>
-                                </select>
-                                <input id={`task-${todo.id}-detail`} type="text" className="add-input-detail-style" defaultValue={todo.detail} placeholder="タスク詳細"  maxLength='50' onChange={onChangeEditTaskDetailText} />
-                                <p className="task-date-style">期限：{todo.date}</p>
-                                <p className="task-created-at-style">作成日：{todo.createdAt}</p>
-                                <button type="button" className="task-delete-button-style" onClick={() => {onClickUpdateTask(todo.id)}} >確定</button>
-                                <button type="button" className="task-delete-button-style" onClick={() => {onClickBackEdit(todo.id)}} >戻す</button>
-                            </div>
-                        </li>
-                    )
-                }else{
-                    return (
-                        <li key={index} style={{listStyle: 'none'}}>
-                            <div className={taskStyle}>
-                                <p className="task-id-style">{todo.id}</p>
-                                <p id={`task-${todo.id}-title`} className="task-title-style">{todo.title}</p>
-                                <select 
-                                    id={`task-${todo.id}-status`} 
-                                    className="task-status-style"
-                                    value={todo.status} onChange={() => {onChangeExistingTaskStatus(todo.id, document.getElementById(`task-${todo.id}-status`).value)}}
-                                >
-                                    <option value="未着手">未着手</option>
-                                    <option value="進行中">進行中</option>
-                                    <option value="完了">完了</option>
-                                </select>
-                                <p id={`task-${todo.id}-detail`} className="task-detail-style">{todo.detail}</p>
-                                <p className="task-date-style">期限：{todo.date}</p>
-                                <p className="task-created-at-style">作成日：{todo.createdAt}</p>
-                                <button type="button" className="task-delete-button-style" onClick={() => {onClickDelete(todo.id)}} >削除</button>
-                                <button type="button" className="task-delete-button-style" onClick={() => {onClickEdit(todo.id)}} >編集</button>
-                                <div>
-                                    <button id={todo.id} className="task-delete-button-style" onClick={onClickOpenComment}>コメント</button>
-                                    <Modal isOpen={modalIsOpen}>
-                                    <button onClick={() => setIsOpen(false)}>閉じる</button>
-                                    <div>
-                                        <input type="text" value={commentContentText} onChange={onChangeCommentContentText} />
-                                        <button type="button" onClick={() => {onClickAddComment(activeCommentParent)}}>コメントする</button>
-                                    </div>
-                                    <h2>コメント一覧</h2>
-                                    {comments.map((comment, index)=>{
-                                        return (
-                                            <div>
-                                                {activeCommentParent === comment.parentId && <><p>{comment.content}</p><hr /></>}
-                                            </div>
-                                        )
-                                    })
-                                    }
-                                    </Modal>
-                                </div>
-                            </div>
-                        </li>
-                    )
-                }
-            })}
-        </ul>
-        </div>
-    )
-}
+  const {
+    todos,
+    onChangeExistingTaskStatus,
+    onClickDelete,
+    ON_EDIT_FLG_TRUE,
+    onClickEdit,
+    onClickBackEdit,
+    onChangeEditTaskTitleText,
+    onChangeEditTaskDetailText,
+    onClickUpdateTask,
+    modalIsOpen,
+    setIsOpen,
+    comments,
+    onClickAddComment,
+    commentContentText,
+    onChangeCommentContentText,
+    onClickOpenComment,
+    activeCommentParent,
+  } = props;
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => {
+          let taskStyle = {};
+          if (todo.status === "未着手") {
+            taskStyle = "status-not-started-yet-task-style";
+          } else if (todo.status === "進行中") {
+            taskStyle = "status-in-progress-task-style";
+          } else {
+            taskStyle = "status-complete-task-style";
+          }
+          if (todo.editFlg === ON_EDIT_FLG_TRUE) {
+            return (
+              <li key={index} style={{ listStyle: "none" }}>
+                <div className={taskStyle}>
+                  <p className="task-id-style">{todo.id}</p>
+                  <input
+                    id={`task-${todo.id}-title`}
+                    type="text"
+                    className="add-input-style"
+                    defaultValue={todo.title}
+                    placeholder="タイトル"
+                    maxLength="10"
+                    onChange={onChangeEditTaskTitleText}
+                  />
+                  <select
+                    id={`task-${todo.id}-status`}
+                    className="task-status-style"
+                    value={todo.status}
+                    onChange={() => {
+                      onChangeExistingTaskStatus(
+                        todo.id,
+                        document.getElementById(`task-${todo.id}-status`).value
+                      );
+                    }}
+                  >
+                    <option value="未着手">未着手</option>
+                    <option value="進行中">進行中</option>
+                    <option value="完了">完了</option>
+                  </select>
+                  <input
+                    id={`task-${todo.id}-detail`}
+                    type="text"
+                    className="add-input-detail-style"
+                    defaultValue={todo.detail}
+                    placeholder="タスク詳細"
+                    maxLength="50"
+                    onChange={onChangeEditTaskDetailText}
+                  />
+                  <p className="task-date-style">期限：{todo.date}</p>
+                  <p className="task-created-at-style">
+                    作成日：{todo.createdAt}
+                  </p>
+                  <button
+                    type="button"
+                    className="task-delete-button-style"
+                    onClick={() => {
+                      onClickUpdateTask(todo.id);
+                    }}
+                  >
+                    確定
+                  </button>
+                  <button
+                    type="button"
+                    className="task-delete-button-style"
+                    onClick={() => {
+                      onClickBackEdit(todo.id);
+                    }}
+                  >
+                    戻す
+                  </button>
+                </div>
+              </li>
+            );
+          } else {
+            return (
+              <li key={index} style={{ listStyle: "none" }}>
+                <div className={taskStyle}>
+                  <p className="task-id-style">{todo.id}</p>
+                  <p id={`task-${todo.id}-title`} className="task-title-style">
+                    {todo.title}
+                  </p>
+                  <select
+                    id={`task-${todo.id}-status`}
+                    className="task-status-style"
+                    value={todo.status}
+                    onChange={() => {
+                      onChangeExistingTaskStatus(
+                        todo.id,
+                        document.getElementById(`task-${todo.id}-status`).value
+                      );
+                    }}
+                  >
+                    <option value="未着手">未着手</option>
+                    <option value="進行中">進行中</option>
+                    <option value="完了">完了</option>
+                  </select>
+                  <p
+                    id={`task-${todo.id}-detail`}
+                    className="task-detail-style"
+                  >
+                    {todo.detail}
+                  </p>
+                  <p className="task-date-style">期限：{todo.date}</p>
+                  <p className="task-created-at-style">
+                    作成日：{todo.createdAt}
+                  </p>
+                  <button
+                    type="button"
+                    className="task-delete-button-style"
+                    onClick={() => {
+                      onClickDelete(todo.id);
+                    }}
+                  >
+                    削除
+                  </button>
+                  <button
+                    type="button"
+                    className="task-delete-button-style"
+                    onClick={() => {
+                      onClickEdit(todo.id);
+                    }}
+                  >
+                    編集
+                  </button>
+                  <div>
+                    <button
+                      id={todo.id}
+                      className="task-delete-button-style"
+                      onClick={onClickOpenComment}
+                    >
+                      コメント
+                    </button>
+                    <Modal isOpen={modalIsOpen}>
+                      <button onClick={() => setIsOpen(false)}>閉じる</button>
+                      <div>
+                        <input
+                          type="text"
+                          value={commentContentText}
+                          onChange={onChangeCommentContentText}
+                        />
+                        <button
+                          type="button"
+                          onClick={() => {
+                            onClickAddComment(activeCommentParent);
+                          }}
+                        >
+                          コメントする
+                        </button>
+                      </div>
+                      <h2>コメント一覧</h2>
+                      {comments.map((comment, index) => {
+                        return (
+                          <div>
+                            {activeCommentParent === comment.parentId && (
+                              <>
+                                <p>{comment.content}</p>
+                                <hr />
+                              </>
+                            )}
+                          </div>
+                        );
+                      })}
+                    </Modal>
+                  </div>
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+};
