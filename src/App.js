9464999e6fe9b5989dfe43a33c3d32c4dd83ccce
JunHatanaka <nextseason6@gmail.com>
2022-03-25 23:24:26 +0900

    Fix: SortButtonを疎結合にする

--- src/App.js
+++ src/App.js
@@ -192,29 +192,6 @@ export const App = () => {
     setActiveCommentParent(event.target.id);
   };
 
-
-  // sort関連
-  const sortByIdAscending = () => {
-    let newTodos = [...todos];
-    newTodos = newTodos.sort((a, b) => a.id - b.id);
-    setTodos(newTodos);
-  };
-
-  const sortByIdDescending = () => {
-    let newTodos = [...todos];
-    newTodos = newTodos.sort((a, b) => b.id - a.id);
-    setTodos(newTodos);
-  };
-
-  const sortByStatusAscending = () => {
-    let newTodos = [...todos];
-    const completeArray = newTodos.filter((item) => item.status === "完了");
-    const progressArray = newTodos.filter((item) => item.status === "進行中");
-    const notYetArray = newTodos.filter((item) => item.status === "未着手");
-    newTodos = progressArray.concat(notYetArray, completeArray);
-    setTodos(newTodos);
-  };
-
   // filter関連
   const filterDisable = () => {
     const newTodos = [...dbMock];
@@ -252,11 +229,7 @@ export const App = () => {
         date={date}
         handleChange={handleChange}
       />
-      <SortButton
-        sortByIdDescending={sortByIdDescending}
-        sortByIdAscending={sortByIdAscending}
-        sortByStatusAscending={sortByStatusAscending}
-      />
+      <SortButton todos={todos} setTodos={setTodos} />
       <FilterButton
         filterDisable={filterDisable}
         filterProgress={filterProgress}
--- src/components/SortButton.jsx
+++ src/components/SortButton.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
 
 export const SortButton = (props) => {
-    const {sortByIdDescending, sortByIdAscending, sortByStatusAscending} = props;
-    return (
-        <div className="sortButtons sort-button-area-style">
-            <button className="sort-button-style" onClick={sortByIdDescending}>id降順</button>
-            <button className="sort-button-style" onClick={sortByIdAscending}>id昇順</button>
-            <button className="sort-button-style" onClick={sortByStatusAscending}>ステータス順 進行中->未着手->完了</button>
-        </div>
-    )
-}
+  const { todos, setTodos } = props;
+  const sortByIdAscending = () => {
+    let newTodos = [...todos];
+    newTodos = newTodos.sort((a, b) => a.id - b.id);
+    setTodos(newTodos);
+  };
+
+  const sortByIdDescending = () => {
+    let newTodos = [...todos];
+    newTodos = newTodos.sort((a, b) => b.id - a.id);
+    setTodos(newTodos);
+  };
+
+  const sortByStatusAscending = () => {
+    let newTodos = [...todos];
+    const completeArray = newTodos.filter((item) => item.status === "完了");
+    const progressArray = newTodos.filter((item) => item.status === "進行中");
+    const notYetArray = newTodos.filter((item) => item.status === "未着手");
+    newTodos = progressArray.concat(notYetArray, completeArray);
+    setTodos(newTodos);
+  };
+  return (
+    <div className="sortButtons sort-button-area-style">
+      <button className="sort-button-style" onClick={sortByIdDescending}>
+        id降順
+      </button>
+      <button className="sort-button-style" onClick={sortByIdAscending}>
+        id昇順
+      </button>
+      <button className="sort-button-style" onClick={sortByStatusAscending}>
+        ステータス順 進行中→未着手→完了
+      </button>
+    </div>
+  );
+};
